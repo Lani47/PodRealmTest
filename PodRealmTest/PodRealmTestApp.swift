8fead7b4857eb62375a31800c3826e3475a0dfb3
Lani47 <21cm0147@jec.ac.jp>
2022-11-11 10:31:38 +0900

    Initial Commit

---
+++ PodRealmTest/PodRealmTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodRealmTestApp.swift
+//  PodRealmTest
+//
+//  Created by cmStudent on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct PodRealmTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
